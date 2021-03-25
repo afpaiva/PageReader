d913690c9d577b6d2bf257b0db4734cda546102e
afpaiva <andrepaiva.arq@gmail.com>
2021-03-25 16:42:23 -0300

    insert textarea to be read in PT-BR

--- reader.js
+++ reader.js
@@ -2,12 +2,8 @@
 const voice = speechSynthesis.getVoices().filter((voice) => voice.lang.includes("pt-BR"))[0];
 
 function play(){
-    const frase = document.getElementById('read').innerText;
+    const frase = document.getElementById('read').value;
     const utterance = new SpeechSynthesisUtterance(frase);
     utterance.voice = voice;
     speechSynthesis.speak(utterance);
-}
-
-function stop(){
-    speechSynthesis.stop(utterance);
 }
